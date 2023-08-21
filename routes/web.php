b658707726db5bef761a948dcde2a703e01b3558
Code Developer <139529770+Wanguki0807@users.noreply.github.com>
2023-08-21 12:42:09 -0500

    update

--- routes/web.php
+++ routes/web.php
@@ -13,20 +13,3 @@ Auth::routes(['verify'=> true]);
 Route::view('/{path?}', 'reactapp')
     ->where('path', '.*');
 
-// Route::get('/email/verify', function () {
-//         return view('auth.verify');
-//     })->middleware('auth')->name('verification.notice');
-
-// Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-//         $request->fulfill();
-     
-//         return redirect('auth.register');
-//     })->middleware(['auth', 'signed'])->name('verification.verify');
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
